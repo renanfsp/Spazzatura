b0b3cc26c97ce057c9542052f67216b0b934ab30
Renan <renanplays@outlook.com.br>
2025-06-02 15:44:48 -0300

    fix users table

--- database/migrations/2025_06_02_152012_add_document_role_to_users_table.php
+++ database/migrations/2025_06_02_152012_add_document_role_to_users_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('cpf')->unique()->nullable();
-            $table->string('cnpj')->unique()->nullable();
+            $table->string('document')->unique();
             $table->enum('role', ['coletor', 'comercio', 'cooperativa'])->default('coletor');
         });
     }
@@ -25,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['cpf', 'cnpj', 'role']);
+            $table->dropColumn(['document', 'role']);
         });
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+// use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,45 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        DB::table("users")->insert([
+            'name' => 'João Silva',
+            'email' => 'joao.silva@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('123456789'),
+            'remember_token' => Str::random(60),
+            'document' => '12.345.678/0001-99',
+            'role' => 'cooperativa',
+            'created_at' => now(),
+            'updated_at'=> now(),
+        ]);
+
+        DB::table("users")->insert([
+            'name' => 'Carlos Souza',
+            'email' => 'carlos.souza@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('123456789'),
+            'remember_token' => Str::random(60),
+            'document' => '321.654.987-00',
+            'role' => 'coletor',
+            'created_at' => now(),
+            'updated_at'=> now(),
+        ]);
+
+        DB::table("users")->insert([
+            'name' => 'Maria Oliveira',
+            'email' => 'maria.oliveira@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('123456789'),
+            'remember_token' => Str::random(60),
+            'document' => '98.765.432/0001-00',
+            'role' => 'comercio',
+            'created_at' => now(),
+            'updated_at'=> now(),
         ]);
     }
 }
