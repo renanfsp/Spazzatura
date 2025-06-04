126b6a139fd920c188f34fc392b91e3ed4c6e7da
Renan <renanplays@outlook.com.br>
2025-06-04 12:36:25 -0300

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,19 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::get('dashboard', function () {
-//         return Inertia::render('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard/comercio', function () {
+        return Inertia::render('dashboard/comercio');
+    })->middleware('comercio')->name('dashboard.comercio');
 
-Route::get('/dashboard/comercio', function () {
-    return Inertia::render('dashboard/comercio');
-})->middleware(['auth', 'comercio'])->name('dashboard.comercio');
+    Route::get('/dashboard/cooperativa', function () {
+        return Inertia::render('dashboard/cooperativa');
+    })->middleware('cooperativa')->name('dashboard.cooperativa');
 
-Route::get('/dashboard/cooperativa', function () {
-    return Inertia::render('dashboard/cooperativa');
-})->middleware(['auth', 'cooperativa'])->name('dashboard.cooperativa');
-
-Route::get('/dashboard/coletor', function () {
-    return Inertia::render('dashboard/coletor');
-})->middleware(['auth','coletor'])->name('dashboard.coletor');
+    Route::get('/dashboard/coletor', function () {
+        return Inertia::render('dashboard/coletor');
+    })->middleware('coletor')->name('dashboard.coletor');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
